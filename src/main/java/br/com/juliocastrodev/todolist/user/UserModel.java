732489cd5df2cf7d3635b14cdf34dae0590f6ef0
Julio <juliocastroti@gmail.com>
2023-10-09 12:16:48 -0300

    Create model and controller User

---
+++ src/main/java/br/com/juliocastrodev/todolist/user/UserModel.java
@@ -0,0 +1,29 @@
+package br.com.juliocastrodev.todolist.user;
+
+public class UserModel {
+    private String username, name, password;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
