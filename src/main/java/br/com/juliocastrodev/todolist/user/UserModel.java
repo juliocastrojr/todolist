51b1320c1c5b025427186d2d4eae820c3cdcea8d
Julio <juliocastroti@gmail.com>
2023-10-10 14:59:02 -0300

    Integração do banco de memória com a aplicação

--- src/main/java/br/com/juliocastrodev/todolist/user/UserModel.java
+++ src/main/java/br/com/juliocastrodev/todolist/user/UserModel.java
@@ -1,29 +1,30 @@
 package br.com.juliocastrodev.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
-    private String username, name, password;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @Column(unique = true)
+    private String username;
+    
+    private String name;
+    private String password;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
