080b7a47f26dcae20fd9bd715f73bb38f9cadf50
Julio <juliocastroti@gmail.com>
2023-10-15 13:32:51 -0300

    Realizando tratativa do erro quando o title excede o máximo de caracteres permitido

--- src/main/java/br/com/juliocastrodev/todolist/task/TaskModel.java
+++ src/main/java/br/com/juliocastrodev/todolist/task/TaskModel.java
@@ -39,4 +39,13 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+
+        if (title.length() > 50) {
+            throw new Exception("O campo title deve ter até 50 caracteres");
+        }
+
+        this.title = title;
+    }
 }
